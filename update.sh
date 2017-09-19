bf89fe4874815d853a69a906888762dad77a5c62
Alexander Günsche <mail@lxg.de>
2017-09-19 16:54:23 +0200

    split app images into prod and dev tags

--- update.sh
+++ update.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+set -e
+
 # A helper script to build the images and push them to the registry
 
-for img in base app db; do
-    docker build -t agitation/$img:latest $img
+docker build -t agitation/base:latest base
+docker build -t agitation/db:latest db
+docker build --build-arg SYMFONY_ENV=dev -t agitation/app:dev app
+docker build --build-arg SYMFONY_ENV=prod -t agitation/app:prod app
 
-    # NOTE: For pushing, `docker login` must be run
-    [ "$1" = "push" ] && docker push agitation/$img:latest
-done
+if [ "$1" = "push" ]; then
+    docker push agitation/base
+    docker push agitation/db
+    docker push agitation/app:prod
+    docker push agitation/app:dev
+fi
