680d3035ff9e1205eefb766e1f67d6fa3f6b88a0
Alexander Günsche <mail@lxg.de>
2017-09-19 12:58:25 +0200

    make  optional

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # A helper script to build the images and push them to the registry
-# NOTE: You must run `docker login` before this script, otherwise pushing won’t work
 
 for img in base app db; do
     docker build -t agitation/$img:latest $img
-    docker push agitation/$img:latest
+
+    # NOTE: For pushing, `docker login` must be run
+    [ "$1" = "push" ] && docker push agitation/$img:latest
 done
