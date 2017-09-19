424a96aa4bb82ac5031391cc35929bd74cbb4d73
Alexander Günsche <mail@lxg.de>
2017-09-19 11:11:02 +0200

    set default password for DB user

--- db/dist/usr/local/sbin/init.db.sh
+++ db/dist/usr/local/sbin/init.db.sh
@@ -4,4 +4,4 @@ until mysql -e "SELECT 1"; do
     sleep 0.2
 done
 
-mysql -e "CREATE DATABASE IF NOT EXISTS app; CREATE USER IF NOT EXISTS app; GRANT ALL ON app.* TO app"
+mysql -e "CREATE DATABASE IF NOT EXISTS app; CREATE USER IF NOT EXISTS app; GRANT ALL ON app.* TO app; SET PASSWORD FOR app = PASSWORD('app');"
